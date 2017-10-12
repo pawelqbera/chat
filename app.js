2834b0ae9698958cd406e10606dfbffdaff2c3bd
Paweł Kubera <pawel.kubera@getresponse.com>
2017-10-12 18:44:48 +0200

    fix issues add API and local starage

--- app.js
+++ app.js
@@ -9,86 +9,127 @@
 	// czat - rozmowa - jest tylko 1 - w danej chwili //DONE
 	// skrypt powoduje, że okienka się same pojawiają na stronie i sa zafixowane do bottom; jedno z lewej - drugie z prawej //DONE
 
+	//BUGI:
+	// - pierwsze okno czatu tworzy się 2 razy //DONE
+	// - event listener wywoluje sie trzy razy // DONE
+	// - poprawne synchro nowych wiadomosci między oknami
+	// - poprawne wyswietlanie sender nameow (wszędzie jest johnny)
+
+	// dodać bazę z session storage
+
 	const model = {
+		chatStorage: localStorage,
 		chats: [
 			{
 				id: 'a',
 			}
 		],
-		users: [
-			{
-				name: 'johnny',
-				id: 'a',
-			}, {
-				name: 'paul',
-				id: 'b',
-			}
-		],
-		messages: [
-			{
-				chatId: 'a',
-				userId: 'b',
-				createDate: 1507672886403,
-				message: 'Hello Johhny!',
-			}, {
-				chatId: 'a',
-				userId: 'a',
-				createDate: 1507672937750,
-				message: 'Hi paul. Whats up?',
-			}, {
-				chatId: 'a',
-				userId: 'b',
-				createDate: 1507673000694,
-				message: 'Howr you doing Johnny?',
-			}, {
-				chatId: 'a',
-				userId: 'a',
-				createDate: 1507673014063,
-				message: 'Great! Thanks!',
-			},
-		]
-	};
-
-	const chatController = {
-		getChatMessages: function(chatId) {
-			return model.messages.filter(function(message) {
-				return message.chatId === chatId;
-			});
+		//Local Storage API
+		fetchMessagesFromStorage: function() {
+			return JSON.parse(this.chatStorage.getItem('messages')) || [];
 		},
-		getUserById: function(userId) {
-			return model.users.find(function(user) {
-				return user.id = userId;
+		setMessagesToStorage: function(messages) {
+			this.chatStorage.setItem('messages', JSON.stringify(messages));
+		},
+		fetchUsersFromStorage: function() {
+			return JSON.parse(this.chatStorage.getItem('users')) || [];
+		},
+		setUsersToStorage: function(users) {
+			this.chatStorage.setItem('users', JSON.stringify(users));
+		},
+
+		// Model API
+		setMessage: function(message) {
+			var messageData = this.fetchMessagesFromStorage();
+
+			messageData.push(message);
+			this.setMessagesToStorage(messageData);
+		},
+		getMessages: function(chatId) {
+			var messageData = this.fetchMessagesFromStorage();
+			var messages = messageData ? messageData.filter(function(message) {
+				return message.chatId === chatId;
+			}) : [];
+
+			return messages;
+		},
+		getUsers: function() {
+			var userData = this.fetchUsersFromStorage();
+			var users = userData ? userData : [];
+
+			return users;
+		},
+ 		getUserById: function(userId) {
+			var userData = this.fetchUsersFromStorage();
+
+			return userData.find(function(user) {
+				return user.id === userId;
 			})
 		},
-		setMessage: function(message) {
-			model.messages.push(message);
-			chat.render();
+		setUser: function(name) {
+			var userData = this.fetchUsersFromStorage();
+			var user = {
+				id: userData.length,
+				name: name
+			}
+
+			userData.push(user);
+			this.setUsersToStorage(userData);
 		}
 	};
 
-	function Chat(userId) {
+	const chatController = {
+		fetchChatMessages: function(chatId) {
+			return model.getMessages(chatId);
+		},
+		fetchUsers: function() {
+			return model.getUsers();
+		},
+		fetchUserById: function(userId) {
+			return model.getUserById(userId);
+		},
+		addMessage: function(message) {
+			model.setMessage(message);
+		},
+		addUser: function(name) {
+			model.setUser(name); 
+		}
+	};
+
+	function Chat(userId, recipientId) {
 		this.userId = userId;
+		this.recipientId = recipientId;
 	}
 
 	Chat.prototype.getMessageInput = function() {
-		return document.querySelector('#chat-input-a');
+		return document.querySelector('#chat-input-' + this.userId);
 	};
 		
-	Chat.prototype.renderChatWindow = function(userId) {
-		var chatMessages = chatController.getChatMessages('a');
+	Chat.prototype.renderChatWindow = function(userId, recipientId) {
+		var chatMessages = chatController.fetchChatMessages('a');
 		var html = '';
+		var chatContainer = document.getElementById("chat-container") ? document.getElementById("chat-container") : createChatContainer();
+
+		function createChatContainer() {
+			var node = document.createElement("div");
+			node.setAttribute("id", "chat-container");
+			document.body.appendChild(node);
+
+			return node;
+		}
 
 		html += '<div class="chat-window chat-window-' + userId + '">';
-		html += '	<div class="chat-recipient">dasdsadas</div>';
-		html += '	<div class="chat-messages">' + this.renderChatMessages(chatMessages) + '</div>';
+		html += '	<div class="chat-recipient">' + chatController.fetchUserById(recipientId).name + '</div>';
+		html += '	<div class="chat-messages" id="chat-messages-' + userId + '">' + this.buildMessages(chatMessages) + '</div>';
 		html += '	<input type="text" id="chat-input-' + userId + '" class="chat-input"/>';
 		html += '</div>';
 
-		document.body.innerHTML += html;
+		chatContainer.innerHTML += html;
 	};
 	
-	Chat.prototype.renderMessage = function(message) {
-		var sender = chatController.getUserById(message.userId);
+	Chat.prototype.buildMessage = function(message) {
+		var sender = chatController.fetchUserById(message.senderId);
+
 		var date = message.createDate;
 		var html = '';
 
@@ -101,86 +142,99 @@
 		return html;
 	};
 
-	Chat.prototype.renderChatMessages = function(messages) {
+	Chat.prototype.buildMessages = function(messages) {
 		var html = '';
 		var _this = this;
 
-		messages.forEach(function(message) {
-			html += '<div>' + this.renderMessage(message) + '</div>';	
+		messages && messages.forEach(function(message) {
+			html += '<div>' + this.buildMessage(message) + '</div>';	
 		}, _this);
 
 		return html;
 	};
-		
+
 	Chat.prototype.bindEvents = function() {
-		document.addEventListener("keypress", function(e) {
-			var input = this.getMessageInput();
-			messageSendHandle(e);
-		}, false);
+		console.log('wywoluje bind events dla:' + this.userId);
+
+		var _this = this;
+		document.addEventListener("keydown", messageSendHandle, false);
 
 		function messageSendHandle(e) {
-			var input = e.currentTarget.querySelector('#chat-input-a');
+			console.log('message send wywola?');
+
+			var input = document.querySelector('#chat-input-' + _this.userId);
 			var key = e.which || e.keyCode;
 			var message = {
 				chatId: model.chats[0].id,
-				userId: 'a',
+				senderId: _this.userId,
+				recipientId: _this.recipientId,
 				createDate: new Date(),
 				message: input.value
 			};
-			var chatMessagesContainer = document.querySelector('.chat-messages');
 
 			console.log('odpalam eventa');
+			console.log('puszczony key:');
+			console.log(e.key);
+			console.log('value w inpucie:');
 			console.log(input.value);
 
 			if (key === 13 && input.value.length !== 0) {
-				chatController.setMessage(message);
+				chatController.addMessage(message);
+				_this.renderNewMessage(message);
+				input.value = '';
 			}
 		}
+	};
+
+	Chat.prototype.renderNewMessage = function(message) {
+		var node = document.createElement("div");
+		node.innerHTML = '<div>' + this.buildMessage(message) + '</div>';
+		document.getElementById('chat-messages-' + this.userId).appendChild(node);
 	}
 
 	Chat.prototype.render = function() {
-		this.renderChatWindow(this.userId);
-	}
+		console.log('wywoluje redner okna dla sendera: ' + this.userId + ' i odbiorcy: ' + this.recipientId);
+		this.renderChatWindow(this.userId, this.recipientId);		
+	};
 	
 	Chat.prototype.init = function() {
 		this.render();
 		this.bindEvents();
-	}
+	};
 
 	const Chats = {
 		chatWindows: [],
 		create: function() {
-			var users = model.users;
+			var users = chatController.fetchUsers();
 			var _this = this;
 
-			users.forEach(function(user) {				
-				_this.chatWindows.push(new Chat(user.id));
-				_this.render(user.id);	
+			users.forEach(function(user, index) {	
+				_this.chatWindows.push(new Chat(user.id, 0));	
 			});
 
-			console.log(this.chatWindows);
+			this.render();
 		},
 		render: function(userId) {
 			var _this = this;
 
 			_this.chatWindows.forEach(function(chatWindow) {
-				chatWindow.render();
+				chatWindow.init();
 			});
 		},
 		init: function() {
 			this.create();
 		}
-	}
+	};
 
 	const chatApp = {
 		init: function() {
-			this.render();
-		},
-		render: function() {
 			Chats.init();
 		}
 	};
 
 	chatApp.init();
+
+	// API
+	window.addUser = chatController.addUser;
 
 })(window);
